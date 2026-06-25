cd2e94f894cb924687372e04075249a3a1d03c28
Ruediger Koenen <ruediger@coach4success.eu>
2026-06-26 00:05:50 +0200

    FIX:device model

--- devices/api/urls.py
+++ devices/api/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path("", device_list),
     path("setup-options/", device_setup_options),
     path("unconfigured/", unconfigured_devices),
-    path("devices/latest/", latest_device_values),
+    path("latest/", latest_device_values),
     path("<int:device_id>/", configure_device),
     path("sankey/", sankey_data),
 ]
