516239891d30e7c7c239d0acb9386a6a54a6ff5a
Ruediger Koenen <ruediger@coach4success.eu>
2026-07-02 18:41:44 +0200

    Add: sankey flow

--- energy/services/energy.py
+++ energy/services/energy.py
@@ -11,39 +11,7 @@ from energy.services.sankey import build_live_sankey
 
 def get_energy_data(user):
 
-    signals = get_ems_signals(user)
-
-    flow = calculate_energy_flow(signals)
-
-    sankey = build_live_sankey(
-        user,
-        flow,
-    )
-
-    unconfigured = Device.objects.filter(
-        home__user=user,
-        configured=False,
-        active=True,
-        pending_delete=False,
-    )
-
+    
     return {
-        "test": "energy.py läuft",
-        "signals": signals,
-        "flow": flow,
-        "sankey": sankey,
-        "devices": {
-            "unconfigured": [
-                {
-                    "id": d.id,
-                    "identifier": d.identifier,
-                    "name": (
-                        d.config.name
-                        if hasattr(d, "config") and d.config.name
-                        else d.identifier
-                    ),
-                }
-                for d in unconfigured
-            ]
+            "test": "ICH BIN ENERGY.PY"
         }
-    }
