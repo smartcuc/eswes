75f041e35153e5d6fa5e26f8c98c35d8f89c9b69
Ruediger Koenen <ruediger@coach4success.eu>
2026-07-03 03:39:11 +0200

    Update: sankey flow

--- devices/api/serializers.py
+++ devices/api/serializers.py
@@ -39,11 +39,11 @@ class FloorSerializer(serializers.ModelSerializer):
 class DeviceConfigSerializer(serializers.ModelSerializer):
 
     
-    # display_name = serializers.CharField(
-    #         source="name",
-    #         required=False,
-    #         allow_blank=True,
-    #     )
+    display_name = serializers.CharField(
+            source="name",
+            required=False,
+            allow_blank=True,
+        )
 
 
     # READ
