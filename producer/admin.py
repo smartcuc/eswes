4b832cde4b3c6cc07ed5cfffe22214d7328d2447
Ruediger Koenen <ruediger@coach4success.eu>
2026-07-19 01:11:11 +0200

    add: producer

--- producer/admin.py
+++ producer/admin.py
@@ -1,36 +1,36 @@
-#
-#
-#
+# #
+# #
+# #
 
-from django.contrib import admin
+# from django.contrib import admin
 
-from producer.models import (
-    GeneratorSystem,
-    GeneratorString,
-)
-
-
-class GeneratorStringInline(admin.TabularInline):
-    model = GeneratorString
-    extra = 1
+# from producer.models import (
+#     GeneratorSystem,
+#     GeneratorString,
+# )
 
 
-@admin.register(GeneratorSystem)
-class GeneratorSystemAdmin(admin.ModelAdmin):
+# class GeneratorStringInline(admin.TabularInline):
+#     model = GeneratorString
+#     extra = 1
 
-    list_display = (
-        "name",
-        "home",
-        "system_type",
-        "peak_power_kw",
-        "active",
-    )
 
-    list_filter = (
-        "system_type",
-        "active",
-    )
+# @admin.register(GeneratorSystem)
+# class GeneratorSystemAdmin(admin.ModelAdmin):
 
-    inlines = [
-        GeneratorStringInline,
-    ]
+#     list_display = (
+#         "name",
+#         "home",
+#         "system_type",
+#         "peak_power_kw",
+#         "active",
+#     )
+
+#     list_filter = (
+#         "system_type",
+#         "active",
+#     )
+
+#     inlines = [
+#         GeneratorStringInline,
+#     ]
