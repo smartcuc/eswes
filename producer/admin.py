6469201fe28a3f0ddf935270bc34f32e0cc7dedc
Ruediger Koenen <ruediger@coach4success.eu>
2026-07-19 03:43:32 +0200

    add: producer

--- producer/admin.py
+++ producer/admin.py
@@ -11,8 +11,19 @@ from producer.models import (
 
 
 class GeneratorStringInline(admin.TabularInline):
+
     model = GeneratorString
+
     extra = 1
+
+    fields = (
+        "name",
+        "module_count",
+        "peak_power_kwp",
+        "orientation",
+        "tilt_deg",
+        "shading_percent",
+    )
 
 
 @admin.register(GeneratorSystem)
@@ -23,12 +34,18 @@ class GeneratorSystemAdmin(admin.ModelAdmin):
         "home",
         "system_type",
         "peak_power_kw",
+        "string_count",
         "active",
     )
 
     list_filter = (
         "system_type",
         "active",
+    )
+
+    search_fields = (
+        "name",
+        "home__name",
     )
 
     inlines = [
